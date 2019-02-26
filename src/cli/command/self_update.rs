a31e57030b156f13fd6929389a009fb42207af07
Marko Kunic <kunicmarko20@gmail.com>
2019-02-26 09:38:07 +0000

    Correct exception messages

--- src/cli/command/self_update.rs
+++ src/cli/command/self_update.rs
@@ -12,42 +12,42 @@ const BINARY_PERMISSIONS: u32 = 0o775;
 impl Command for SelfUpdate {
     fn execute(&self) {
         let mut response = reqwest::get(GITHUB_LATEST_RELEASE)
-            .expect("Failed to fetch current release metadata.");
+            .expect("Failed to fetch the current release metadata.");
 
         let release: Release = response.json()
-            .expect("Failed deserialize release metadata.");
+            .expect("Failed to deserialize the release metadata.");
 
         if release.tag == env!("CARGO_PKG_VERSION") {
             return;
         }
 
         let mut binary = reqwest::get(release.binary_download_url())
-            .expect("Failed to download new binary.");
+            .expect("Failed to download the new binary.");
 
         let current_binary_path = std::env::current_exe()
-            .expect("Failed to get path of current binary.");
+            .expect("Failed to get the path of the current binary.");
 
         let current_binary_path = current_binary_path.to_str()
-            .expect("Failed to convert path of current binary to string.");
+            .expect("Failed to convert the path of the current binary to string.");
 
         std::fs::remove_file(current_binary_path)
-            .expect("Failed to remove old binary.");
+            .expect("Failed to remove the old binary.");
 
         let mut file_content = std::fs::File::create(current_binary_path)
-            .expect("Failed to create new binary.");
+            .expect("Failed to create the new binary.");
 
         std::io::copy(&mut binary, &mut file_content)
-            .expect("Failed to write new binary to the disk.");
+            .expect("Failed to write the new binary to the disk.");
 
         let metadata = file_content.metadata()
-            .expect("Failed get new binary metadata.");;
+            .expect("Failed to get the new binary metadata.");;
 
         let mut permissions = metadata.permissions();
 
         permissions.set_mode(BINARY_PERMISSIONS);
 
         std::fs::set_permissions(current_binary_path, permissions)
-            .expect("Failed to make new binary executable.");
+            .expect("Failed to make the new binary executable.");
     }
 }
 
